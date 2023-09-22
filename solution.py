4235e24b5ce88f4a46f43304934dfc55324c2457
ryg-git <ygretharekar@outlook.com>
2023-09-22 06:24:39 +0530

    lc 230922

--- solution.py
+++ solution.py
@@ -1,51 +1,29 @@
 from collections import Counter, defaultdict, deque, namedtuple
 from functools import reduce
+import heapq
 import string
 from typing import List, Optional
 
 
 class Solution:
-    def shortestPathLength(self, graph: List[List[int]]) -> int:
-
-        n = len(graph)
-
-        vis = set()
-
-        msk = (1 << n) - 1
-
-        Node = namedtuple('Node', ('node', 'mask', 'cost'))
-
-        q = deque()
-
-        ans = float('inf')
-
-        for i in range(n):
-            no = Node(i, (1 << i), 1)
-            q.append(no)
-
-            vis.add((i, (1 << i)))
-
-        while q:
-            nn = q.popleft()
-
-            if msk == nn.mask:
-                return nn.cost - 1
-
+    def isSubsequence(self, s: str, t: str) -> bool:
+        si = 0
+        ti = 0
+        
+        while si < len(s) and ti < len(t):
+            if s[si] == t[ti]:
+                si += 1
+                ti += 1
             else:
-                for adj in graph[nn.node]:
-                    nm = nn.mask | 1 << adj
-
-                    if (adj, nm) not in vis:
-                        q.append(Node(adj, nm, nn.cost + 1))
-                        vis.add((adj, nm))
-
-        return ans
+                ti += 1
+        
+        return si == len(s)
 
 
 def main():
     s = Solution()
 
-    ans = s.shortestPathLength(graph=[[1, 2, 3], [0], [0], [0]])
+    ans = s.isSubsequence(s = "abc", t = "ahbgdc")
 
     print(ans)
 
@@ -54,35 +32,30 @@ main()
 
 
 def solve():
+    n, m, d = [int(x) for x in input().split()]
+    s = list(map(int, input().split()))
 
-    mod = 1_000_000_007
+    po = 0
 
-    n = int(input())
-    arr = list(map(int, input().strip().split()))[:n]
+    e = 1
 
-    dp = [(0, 0) for _ in range(n + 1)]
-
-    ma = -100001
-
-    for i in range(n + 1):
-        dp[i] = (100001, 0)
+    sel = 0
 
     for i in range(1, n + 1):
-        na, nb = dp[i - 1]
-        for j in range(i, n + 1):
-            a = min(na, arr[j - 1])
-            b = nb + arr[j - 1]
-            c = ((-1 * a * b) % mod) * -1 if (a > 0) ^ (b > 0) else a * b % mod
+        if po < m and s[po] < e:
+            e = s[po] + d - 1
+            if po < m - 1:
+                po += 1
+            sel += 1
+        if e == i:
+            e += d - 1
 
-            ma = max(ma,  c)
-            na, nb = dp[j]
-            dp[j] = (a, b)
-
-    return ma
+    return d
 
 
 def main2():
-    n = int(input())
+    # n = int(input())
+    n = 1
     for _ in range(n):
         ans = solve()
         print(ans)
